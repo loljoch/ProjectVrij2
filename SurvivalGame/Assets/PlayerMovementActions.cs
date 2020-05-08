db0b37abea7981361a995e71ad700fa92d640c03
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-05-08 09:28:10 +0200

    Added Packages and working on movement script (forget to commit yesterday) -Added InputManager Package -Added CineMachine Camera Package -Added PlayerMovement Script

---
+++ SurvivalGame/Assets/PlayerMovementActions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementActions : MonoBehaviour
+{
+	public void OnMove(InputAction.CallbackContext _context)
+	{
+		Debug.Log("Moving!");
+	}
+
+	public void OnFire(InputAction.CallbackContext _context)
+	{
+		Debug.Log("Firing!");
+	}
+}
