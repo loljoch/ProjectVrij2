a35fda0f51d6759eed327bc749e70f8256872980
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-06-23 17:25:07 +0200

    -Added Scripts for different enemies still work the same as the clamp enemy

---
+++ SurvivalGame/Assets/Scripts/Characters/Enemies/PufferFishEnemy.cs
@@ -0,0 +1,35 @@
+﻿public class PufferFishEnemy : BaseEnemy
+{
+	private void Update()
+	{
+		if (isAttacking) return;
+
+		float distance = DistanceToPlayer;
+
+		if (distance > spotRange)
+		{
+			anim.SetTrigger(Animations.Idle);
+			StopMoving();
+			return;
+		}
+
+		if (distance < spotRange)
+		{
+			LookAtPlayer();
+		}
+
+		if (distance < baseAttackRange)
+		{
+			if (!TryAttack())
+			{
+				anim.SetTrigger(Animations.Idle);
+			}
+			return;
+		}
+
+		if (distance < movingToPlayerRange)
+		{
+			MoveTowardsPlayer();
+		}
+	}
+}
