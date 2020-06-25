06fea79ea9c489799ef37c90a3e117692c707938
Ruben <rubenhooijer@aol.com>
2020-06-25 23:15:54 +0200

    Weapons

--- SurvivalGame/Assets/Scripts/Weapons/Spear.cs
+++ SurvivalGame/Assets/Scripts/Weapons/Spear.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Spear : MonoBehaviour, IWeapon
 {
@@ -10,12 +11,13 @@ public class Spear : MonoBehaviour, IWeapon
     [SerializeField] private Animations.WeaponAnimation weaponAnimation;
     private string weaponAnimationName;
 
-    [SerializeField] private GameObject throwingSpear;
+    [SerializeField] private Rigidbody throwingSpear;
     public Animator PlayerAnim { get => playerAnim; set => playerAnim = value; }
 
     private Animator playerAnim;
     public float AttackInterval => attackInterval;
     public float attackInterval = 1f;
+    [SerializeField] private float projectileSpeed = 2;
 
     private void Awake()
     {
@@ -24,12 +26,13 @@ public class Spear : MonoBehaviour, IWeapon
 
     public void DoAttackAnimation()
     {
-        FMODUnity.RuntimeManager.PlayOneShot(attackSFX, transform.position);
         playerAnim.Play(weaponAnimationName);
     }
 
     public void Attack()
     {
-        
+        FMODUnity.RuntimeManager.PlayOneShot(attackSFX, transform.position);
+        var spear = Instantiate(throwingSpear, transform.position, transform.rotation);
+        spear.velocity = transform.forward * projectileSpeed;
     }
 }
